e458fe1f1a9caf85448e070e672888ded8f2eb7d
jonathan <jonathan@adeogroup.co.uk>
2020-06-12 18:04:13 +0100

    licence code

--- jonathansblog-featured-image-setter.php
+++ jonathansblog-featured-image-setter.php
@@ -8,6 +8,19 @@
   Version: 0.1
   License: GPL v3 or later
   License URI: https://www.gnu.org/licenses/gpl-3.0.html
+
+  jonathansblog featured image setter is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  any later version.
+ 
+  jonathansblog featured image setter is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+ 
+  You should have received a copy of the GNU General Public License
+  along with jonathansblog featured image setter. If not, see https://www.gnu.org/licenses/gpl-3.0.html.
   */
 
 // include admin page if present
